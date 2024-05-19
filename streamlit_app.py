11d6be859bbcd55e3fd7f9d3939432de7fdcdffa
Hith Thapa <86625989+HithThapa@users.noreply.github.com>
2024-05-19 12:36:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -13,29 +14,6 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be: ", name_on_order)
-
-# # Debugging - Print the contents of st.secrets
-# st.write("Contents of st.secrets:")
-# st.write(st.secrets)
-
-# # Access Snowflake connection details from Streamlit secrets
-# try:
-#     snowflake_user = st.secrets["connections"]["snowflake"]["user"]
-#     snowflake_password = st.secrets["connections"]["snowflake"]["password"]
-#     snowflake_account = st.secrets["connections"]["snowflake"]["account"]
-#     snowflake_warehouse = st.secrets["connections"]["snowflake"]["warehouse"]
-#     snowflake_database = st.secrets["connections"]["snowflake"]["database"]
-#     snowflake_schema = st.secrets["connections"]["snowflake"]["schema"]
-#     st.write("Snowflake connection details loaded successfully.")
-# except KeyError as e:
-#     st.error(f"Missing key in secrets: {e}")
-#     st.stop()
-
-
-
-
-
-
 # session = get_active_session()
 cnx=st.connection("snowflake")
 session = cnx.session()
@@ -51,7 +29,9 @@ if ingredients_list:
     
     for Fruit_Chosen in ingredients_list:
         ingredients_string += Fruit_Chosen + ' '
-        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        # st.text(fruityvice_response.json())
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -66,9 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
-
-# New Section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
